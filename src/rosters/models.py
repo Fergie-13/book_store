8d1595c1b6990ccc92e5c99fc967f6dc1fe448d3
unknown <yanchilenko98@gmail.com>
2022-08-12 23:31:17 +0300

    'startapp'

--- src/rosters/models.py
+++ src/rosters/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Genre(models.Model):
+    name = models.CharField(
+        verbose_name = "Genre name",
+        max_length = 30,
+    )
+    description = models.TextField(
+        verbose_name = "Genre description",
+        blank = True,
+        null = True
+    )
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Author(models.Model):
+    name = models.CharField(
+        verbose_name = "Author's name",
+        max_length = 30,
+    )
+    description = models.TextField(
+        verbose_name = "Author's description",
+        blank = True,
+        null = True
+    )
+
+    def __str__(self) -> str:
+        return self.name
